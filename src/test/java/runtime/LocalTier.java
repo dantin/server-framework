46b35965252d3108ebf203b11ee6ab733b04487c
dantin <chengjie.ding@gmail.com>
2014-11-28 09:29:38 +0800

    add local bootstrap class

---
+++ src/test/java/runtime/LocalTier.java
@@ -0,0 +1,14 @@
+package runtime;
+
+import com.demo2do.server.Bootstrap;
+
+/**
+ * 本地环境启动类
+ */
+public class LocalTier {
+
+    public static void main(String[] args) {
+        Bootstrap.main(args);
+    }
+
+}
