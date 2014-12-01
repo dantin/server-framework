b53f14b58dc4ee2552d9815937a94d328d6053a1
dantin <chengjie.ding@gmail.com>
2014-12-01 22:53:27 +0800

    refactoring protobuf and generation script

--- src/assembly/generate.sh
+++ src/assembly/generate.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-PROTO_DIR=../main/resources/proto
+PROTO_DIR=../main/resources/app
 SRC_DIR=../main/java
 
 rm -rf $SRC_DIR/com/demo2do/entity/protocol/proto/common
 mkdir -p $SRC_DIR/com/demo2do/entity/protocol/proto/common
 
-protoc --proto_path=$PROTO_DIR --java_out=$SRC_DIR $PROTO_DIR/Common.proto
-protoc --proto_path=$PROTO_DIR --java_out=$SRC_DIR $PROTO_DIR/User.proto
-protoc --proto_path=$PROTO_DIR --java_out=$SRC_DIR $PROTO_DIR/AppPb.proto
+protoc --proto_path=$PROTO_DIR/common --java_out=$SRC_DIR $PROTO_DIR/common/common.proto
+protoc --proto_path=$PROTO_DIR/user --proto_path=$PROTO_DIR/common --java_out=$SRC_DIR $PROTO_DIR/user/user.proto
+protoc --proto_path=$PROTO_DIR --proto_path=$PROTO_DIR/user --proto_path=$PROTO_DIR/common --java_out=$SRC_DIR $PROTO_DIR/app.proto
