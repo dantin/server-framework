2a1bc9982af6e7809b1037dd43a2ca063ee00fb4
dantin <chengjie.ding@gmail.com>
2014-12-02 09:45:17 +0800

    add protocol class

---
+++ src/main/java/com/demo2do/netty/Protocol.java
@@ -0,0 +1,22 @@
+package com.demo2do.netty;
+
+/**
+ * 协议
+ */
+public enum Protocol {
+
+    /** Unsupported Protocol */
+    UNKNOWN("unknown"),
+    /** Google Protocol Buffer */
+    PROTOBUF("protobuf");
+
+    private String code;
+
+    private Protocol(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
