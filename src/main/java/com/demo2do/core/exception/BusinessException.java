ece9cdf2fde803d1ca1f98af7d2ba1b6cf99e8f0
dantin <chengjie.ding@gmail.com>
2014-12-02 15:36:46 +0800

    add business exception

---
+++ src/main/java/com/demo2do/core/exception/BusinessException.java
@@ -0,0 +1,22 @@
+package com.demo2do.core.exception;
+
+/**
+ * 业务异常对象
+ *
+ * @author David
+ */
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(Throwable cause) {
+        super(cause);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
